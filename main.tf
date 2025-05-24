f00811388f55ad50be1db5c579818a67509b1e2c
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-05-24 14:30:33 +0530

    Creating EC2

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformEC2"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}

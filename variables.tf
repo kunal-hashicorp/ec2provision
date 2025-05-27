e0d4f85f7e7970cc117b034e7bf8a2a95ee5b51e
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-05-27 14:49:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,3 +15,15 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "iam_role_name" {
+  description = "IAM role name for EC2 instance"
+  type        = string
+  default     = "ec2-instance-role"
+}
+
+variable "iam_instance_profile_name" {
+  description = "IAM instance profile name"
+  type        = string
+  default     = "ec2-instance-profile"
+}

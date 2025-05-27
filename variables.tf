83cf9915c494e881ee6818b60af28a2c5ee0d78c
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-05-27 15:18:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,12 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-  default     = "ami-0c55b159cbfafe1f0" # Replace if needed
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string

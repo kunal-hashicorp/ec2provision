56f67ff3bab83c41ff06adcaade025394e6ea213
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-06-29 15:08:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,11 @@ locals {
   ami_id = "ami-0d016af584f4febe3" # Replace with a valid AMI in your region
 }
 
-# Reference existing IAM instance profile
-data "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-instance-profile"
-}
-
 # EC2 Instance Resource
 resource "aws_instance" "example" {
   ami                    = local.ami_id
   instance_type          = var.instance_type
-  iam_instance_profile   = data.aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile   = "ec2-instance-profile"  # Hardcoded to avoid data source
 
   tags = {
     Name = "TerraformEC2"
